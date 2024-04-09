de2a899eaa27ffbfca0f9bbc241b3aa51a787822
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-04-09 23:02:07 +0300

    Update SafeFile.hpp

--- server/SafeFile.hpp
+++ server/SafeFile.hpp
@@ -1,1 +1,17 @@
+#ifndef SAFEFILE_HPP
+#define SAFEFILE_HPP
+
+#include <string>
+#include <fstream>
+#include <mutex>
+
+class SafeFile {
+public:
+    static void writeToFile(const std::string& filename, const std::string& message);
+
+private:
+    static std::mutex m_mutex;
+};
+
+#endif // SAFEFILE_HPP
 
