34c7c140bf99dfe51fcc618639efade4e00100d3
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-04-09 22:59:42 +0300

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,1 +1,28 @@
+import socket
+import time
+
+def main():
+    host = '10.120.55.208'
+    port = 15000
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((host, port))
+
+        client_socket.sendall(b'Hello')
+
+        wait_response = client_socket.recv(1024)
+        print("Received from server:", wait_response.decode())
+
+        ready_response = client_socket.recv(1024)
+        print("Received from server:", ready_response.decode())
+
+        message = input("Enter your message: ")
+        client_socket.sendall(message.encode())
+
+        ok_response = client_socket.recv(1024)
+        print("Received from server:", ok_response.decode())
+
+
+if __name__ == "__main__":
+    main()
 
