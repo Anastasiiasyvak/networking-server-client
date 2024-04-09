dd21a08029c4f76f22198f8d4e29fc0e0f2abcb3
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-04-09 23:02:33 +0300

    Update SafeFile.cpp

--- server/SafeFile.cpp
+++ server/SafeFile.cpp
@@ -1,1 +1,15 @@
+#include "SafeFile.hpp"
+
+std::mutex SafeFile::m_mutex;
+
+void SafeFile::writeToFile(const std::string& filename, const std::string& message) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    std::ofstream file(filename, std::ios_base::app);
+    if (file.is_open()) {
+        std::time_t now = std::time(nullptr);
+        char timeString[64];
+        strftime(timeString, sizeof(timeString), "%c", std::localtime(&now));
+        file << timeString << ": " << message << std::endl;
+    }
+}
 
